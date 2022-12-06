0b9f7626cbf515c3114736cb20358f321e4f78f8
eskitzis <eskitzis@csd.auth.gr>
2022-12-06 15:04:12 +0200

    master

--- main.py
+++ main.py
@@ -2,32 +2,35 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 from time import strftime
-import base64
 # /////////////////////////////////////////////////////
 # creating window
 window = tk.Tk()
 window2 = tk.Tk()
+window3 = tk.Tk()
 
 window.after(15000, lambda: window.destroy())
 window2.after(15000, lambda: window2.destroy())
+window3.after(15000, lambda: window3.destroy())
 window.resizable(height=None, width=None)
 window2.resizable(height=None, width=None)
+window3.resizable(height=None, width=None)
+
+# getting screen size
+scrW = window.winfo_screenwidth()
+scrH = window.winfo_screenheight()
 
 # setting attribute
-# window.geometry("1920x1080")
-window.geometry(f'{1920}x{1080}+0+0')
-window2.geometry(f'{1920}x{1080}+1920+0')
-window.wm_attributes("-fullscreen", True)
-window2.wm_attributes("-fullscreen", True)
+window.geometry("1920x1080+0+0")
+window2.geometry("1920x1080+%d+0" % scrW)
+window3.geometry("1920x1080-%d-0" % scrW)
+window.attributes("-fullscreen", True)
+#window2.attributes("-fullscreen", True)
+#window3.attributes("-fullscreen", True)
 
 image = Image.open(r"C:\Users\anton\PycharmProjects\networrkers_popup_python\image.png")
 photo_image = ImageTk.PhotoImage(image)
 label = Label(window, image=photo_image)
 label.pack()
-
-label1 = Label(window, image=photo_image)
-label1.pack()
-
 
 # live time
 
@@ -45,4 +48,5 @@ l1.place(relx=1, rely=0, x=-38, y=65, anchor='ne')
 my_time()
 window.mainloop()
 window2.mainloop()
+window3.mainloop()
 # /////////////////////////////////////////////////////
